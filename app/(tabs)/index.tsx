c8fd67ca5f2184c4e5925568b810e7d34577986d
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-03-27 16:07:24 +0200

    Webhook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
 import { styles } from '../../styles/auth.styles';
 import { Link } from 'expo-router';
+import { useAuth } from '@clerk/clerk-expo';
 
 export default function Index() {
+    const { signOut } = useAuth();
+
     return (
         <View style={styles.container}>
-            <Link href={'/notifications'}>Notifications screen</Link>
+            <TouchableOpacity onPress={() => signOut()}>
+                <Text style={{ color: 'white' }}>Sign out</Text>
+            </TouchableOpacity>
         </View>
     );
 }
